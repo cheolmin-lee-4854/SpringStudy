5e82de7b45ae547bf8d10f68cee77d82ca425521
unknown <cheoloffice4854@gmail.com>
2024-08-09 16:28:42 +0900

    테스트 하는 방법

--- spring-prepare/src/main/java/com/sparta/springprepare/SpringPrepareApplication.java
+++ spring-prepare/src/main/java/com/sparta/springprepare/SpringPrepareApplication.java
@@ -3,7 +3,7 @@ package com.sparta.springprepare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication                                                                                                                                                    
 public class SpringPrepareApplication {
 
     public static void main(String[] args) {
